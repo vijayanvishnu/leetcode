5033e2cbd74627bc211767e4c8170768ecc8f2c6
vijayanvishnu <128233361+vijayanvishnu@users.noreply.github.com>
2024-02-24 22:21:53 +0530

    Time: 73 ms (56.86%) | Memory: 122.6 MB (9.80%) - LeetSync

--- 2213-find-all-people-with-secret/find-all-people-with-secret.java
+++ 2213-find-all-people-with-secret/find-all-people-with-secret.java
@@ -1,7 +1,5 @@
 class Solution {
     public List<Integer> findAllPeople(int n, int[][] meetings, int firstPerson) {
-
-        // get index of meetings with same time together 
         Arrays.sort(meetings , (a,b)->Integer.compare(a[2],b[2]));
         ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
         ArrayList<Integer> l = new ArrayList<>();
@@ -21,14 +19,10 @@ class Solution {
             }
         }
         graph.add(l);
-
-        // DISJOINT SET
         djs(n);
         union(0,firstPerson);
         for(int j=0 ;j<graph.size() ; j++){
             HashSet<Integer> set = new HashSet<>();
-
-            // union both person meeting at a time
             for(int e : graph.get(j)){
                 int x = meetings[e][0];
                 int y = meetings[e][1];
@@ -36,13 +30,10 @@ class Solution {
                 set.add(x);
                 set.add(y);
             }
-            // reset person not knowing secret at a time
             for(int e: set){
                 if(find(0)!=find(e)) reset(e);
             }
         }
-
-        // return person who know secret
         List<Integer> list = new ArrayList<>();
         for(int j=0 ;j<n ; j++){
             if(find(0)==find(j)) list.add(j);
@@ -50,8 +41,6 @@ class Solution {
         return list;
         
     }
-    
-    // union find
     int[] parent = new int [100000];
     void djs(int n){
         for(int i=0 ; i<n ; i++){
@@ -73,10 +62,7 @@ class Solution {
         }
         return false;
     }
-
-    // reset function
     void reset(int node){
         parent[node] = node;
     }
-
 }
