a9ed0ce5144ad481f7a2c2a624db418444700c3b
vijayanvishnu <128233361+vijayanvishnu@users.noreply.github.com>
2024-05-12 02:04:56 +0530

    Time: 24 ms (50.00%) | Memory: 97.9 MB (100.00%) - LeetSync

---
+++ 3419-maximum-points-inside-the-square/maximum-points-inside-the-square.java
@@ -0,0 +1,32 @@
+class Solution {
+    private int helper(int points[][] , char str[] , int a){
+        HashSet<Character> set = new HashSet<>();
+        for(int i = 0 ; i < points.length ; i++){
+            int x = Math.abs(points[i][0]);
+            int y = Math.abs(points[i][1]);
+            if(x <= a && y <= a){
+                if(set.contains(str[i])){
+                    return -1;
+                }
+                set.add(str[i]);
+            }
+        }
+        return set.size();
+    }
+    public int maxPointsInsideSquare(int[][] points, String s) {
+        int left = 0;
+        int right = 1_00_000_000_1;
+        int ans = 0;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            int count = helper(points,s.toCharArray(),mid);
+            if(count == -1){
+                right = mid - 1;
+            }else{
+                ans = Math.max(ans,count);
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
