ded6e9623cd92d6ad69e6a73a1141787dc951450
vijayanvishnu <128233361+vijayanvishnu@users.noreply.github.com>
2024-07-24 10:20:50 +0530

    Time: 64 ms (28.70%) | Memory: 62 MB (60.18%) - LeetSync

--- 2438-find-closest-node-to-given-two-nodes/find-closest-node-to-given-two-nodes.java
+++ 2438-find-closest-node-to-given-two-nodes/find-closest-node-to-given-two-nodes.java
@@ -1,7 +1,6 @@
 class Solution {
     public int closestMeetingNode(int[] edges, int src, int dst) {
         Map<Integer,Integer> srcPath = new HashMap<>();
-        Map<Integer,Integer> dstPath = new HashMap<>();
         int dis = 0;
         int curr = src;
         while(curr != -1 && !srcPath.containsKey(curr)){
@@ -9,26 +8,25 @@ class Solution {
             curr = edges[curr];
             dis++;
         }
+        int ans = -1;
         curr = dst;
         dis = 0;
-        while(curr != -1 && !dstPath.containsKey(curr)){
-            dstPath.put(curr,dis);
-            curr = edges[curr];
-            dis++; 
-        }
-        int ans = -1;
-        dis = Integer.MAX_VALUE;
-        for(int i : srcPath.keySet()){
-            if(dstPath.containsKey(i)){
-                int sum = Math.max(srcPath.get(i) , dstPath.get(i));
+        int ansDis = Integer.MAX_VALUE;
+        Set<Integer> visited = new HashSet<>();
+        while(curr != -1 && !visited.contains(curr)){
+            if(srcPath.containsKey(curr)){
+                int maxDis = Math.max(srcPath.get(curr) , dis);
                 // System.out.println(sum + " " + i);
-                if(dis > sum){
-                    dis = sum;
-                    ans = i;
-                }else if(dis == sum){
-                    ans = Math.min(ans,i);
+                if(ansDis > maxDis){
+                    ansDis = maxDis;
+                    ans = curr;
+                }else if(ansDis == maxDis){
+                    ans = Math.min(ans,curr);
                 }
             }
+            visited.add(curr);
+            curr = edges[curr];
+            dis++; 
         }
         return ans;
     }
